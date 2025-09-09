45ae22166fb0ecd7b53161744b2466096cec9ae6
AghaShayan123 <agha.shayan123@gmail.com>
2025-09-10 01:34:16 +0500

    container mfp setup

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,21 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common.js");
+
+const devConfig = {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: "/index.html",
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, devConfig);
